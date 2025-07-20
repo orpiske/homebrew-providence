edf99b5e7233b5a304e9d33b8bb5117e14a5fab9
orpiske <angusyoung@gmail.com>
2025-07-20 09:03:44 +0000

    providence-collector v0.0.11

--- Formula/providence-collector.rb
+++ Formula/providence-collector.rb
@@ -1,30 +1,30 @@
-# Generated with JReleaser 1.19.0 at 2025-07-12T08:29:14.451714736Z
+# Generated with JReleaser 1.19.0 at 2025-07-20T09:03:36.378911991Z
 
 class ProvidenceCollector < Formula
   desc "Providence Collector"
   homepage "https://github.com/orpiske/providence"
-  url "https://github.com/orpiske/providence/releases/download/v0.0.10/providence-collector-0.0.10-jar-with-dependencies.jar", :using => :nounzip
-  version "0.0.10"
-  sha256 "81049b5560fb77d1e5b0b88085c67979ad1f8e9a9119f2695d3115993dc87329"
+  url "https://github.com/orpiske/providence/releases/download/v0.0.11/providence-collector-0.0.11-jar-with-dependencies.jar", :using => :nounzip
+  version "0.0.11"
+  sha256 "f73f0ed16d213d5d5ee20844534d86b6860a322ab9157d3ccd2053f6b4ea7cf9"
   license "Apache-2.0"
 
   depends_on "openjdk@21"
 
   def install
-    libexec.install "providence-collector-0.0.10-jar-with-dependencies.jar"
+    libexec.install "providence-collector-0.0.11-jar-with-dependencies.jar"
 
     bin.mkpath
     File.open("#{bin}/providence-collector", "w") do |f|
       f.write <<~EOS
         #!/bin/bash
         export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/providence-collector-0.0.10-jar-with-dependencies.jar "$@"
+        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/providence-collector-0.0.11-jar-with-dependencies.jar "$@"
       EOS
     end
   end
 
   test do
     output = shell_output("#{bin}/providence-collector --version")
-    assert_match "0.0.10", output
+    assert_match "0.0.11", output
   end
 end
